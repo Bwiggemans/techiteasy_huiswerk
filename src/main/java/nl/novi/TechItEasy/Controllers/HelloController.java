707340d277180869cec0d1690d993b240a040e3e
Bwiggemans <b.wiggemans@home.nl>
2021-11-25 15:41:17 +0100

    HelloController gemaakt om te testen deze werkt

---
+++ src/main/java/nl/novi/TechItEasy/Controllers/HelloController.java
@@ -0,0 +1,18 @@
+package nl.novi.TechItEasy.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping(value = "/hello")
+    public String sayHello(){
+        return "Hello World";
+    }
+
+    @GetMapping(value = "/goodbye")
+        public String goodbye(){
+        return "Goodbye! Have a nice day";
+    }
+}
