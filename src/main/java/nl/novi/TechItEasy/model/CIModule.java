dd3a3b4f9d4af5a430e9e94ac747f062fc8715c7
Bwiggemans <b.wiggemans@home.nl>
2021-12-07 10:22:46 +0100

    status einde les 25 nov alles werkt

--- src/main/java/nl/novi/TechItEasy/model/CIModule.java
+++ src/main/java/nl/novi/TechItEasy/model/CIModule.java
@@ -25,39 +25,30 @@ public class CIModule {
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getType() {
         return type;
     }
-
     public void setType(String type) {
         this.type = type;
     }
-
     public double getPrice() {
         return price;
     }
-
     public void setPrice(double price) {
         this.price = price;
     }
-
     public List<Television> getTelevisions() {
         return televisions;
     }
-
     public void setTelevisions(List<Television> televisions) {
         this.televisions = televisions;
     }
